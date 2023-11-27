529410d311086e5eccc8fcb2787bb06653270cf8
Sneha Pandit <snehapandit526@gmail.com>
2023-11-27 20:58:10 +0530

    first commit

---
+++ WeatherApp--/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import NavSection from "./components/NavSection";
+function App() {
+  return (
+    <section>
+      <NavSection />
+    </section>
+  );
+}
+
+export default App;
